6c1bdfb16b01ae45139f13e6c6dfb7c755c759f8
Mio u <121242737+MioNya4@users.noreply.github.com>
2023-11-20 16:41:54 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,6 +7,8 @@ make
 # install daemon and app to menu
 cp usb_wakeup_configuration /usr/bin/
 cp ../usb_wakeup_daemon.sh /usr/bin/
+chmod +x /usr/bin/usb_wakeup_daemon.sh
+
 cp ../usb_wakeup.desktop /etc/xdg/autostart
 cp ../usb_wakeup_tool.desktop /usr/share/applications/
 
@@ -15,6 +17,10 @@ cp ../usb_wakeup_daemon.policy /usr/share/polkit-1/actions/
 chmod 644 /usr/share/polkit-1/actions/usb_wakeup_daemon.policy
 cp ../usb_wakeup_tool.policy /usr/share/polkit-1/actions/
 chmod 644 /usr/share/polkit-1/actions/usb_wakeup_tool.policy
+
+echo run usb wakeup tool from app menu to set up the first time
+echo restart x session to run daemon that will watch and reapply devices automatically
+echo dm me in https://t.me/l33tgraceful if anything is broken (i suspect my pkexec hack is broken but i check myself :<)
 
 
 
